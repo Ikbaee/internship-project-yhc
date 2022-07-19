62d1a7f356f7dfbe3ad2204411cf906f45b5df9f
Muhammad Iqbal <ikozxiqbal@gmail.com>
2022-07-19 19:57:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -74,16 +74,19 @@ $sql = mysqli_query($conn,$query);
                 
                 
     </div>
+    
     </tbody>
 
+
+
+    </table>
 <script>
     $(document).ready(function(){
         $('#tabel-data').DataTable();
     });
 </script>
-
-    </table>
-    
+<h4>Create by:</h4>
+    <h4>Muhammad Iqbal</h4>    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
